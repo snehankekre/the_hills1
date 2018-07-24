8eb801a93cfb1843b8042a702337b748f9620cac
Snehan Kekre <snehan@minerva.kgi.edu>
2018-07-24 19:29:42 +0800

    fix typo

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,7 @@ require 'mechanize'
 require 'scraperwiki'
 
 agent = Mechanize.new
-agent.ssl_version = :TLSv2
+agent.ssl_version = :TLSv1_2
 enquiry_url = "https://epathway.thehills.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx"
 
 # Get the main page and ask for DAs
