d4b811fc6fad4f2ff1e537ab68a34726a9626d3b
Snehan Kekre <snehan@minerva.kgi.edu>
2018-07-25 00:08:09 +0800

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -14,8 +14,10 @@ require 'scraperwiki'
 end'''
 
 
-agent = Mechanize.new
-agent.ssl_version = :SSLv3_server
+#agent = Mechanize.new
+agent = OpenSSL::SSL::SSLContext.new
+agent.ssl_version = :TLSv1_2
+#agent.ssl_version = :SSLv3_server
 enquiry_url = "https://epathway.thehills.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx"
 
 # Get the main page and ask for DAs
