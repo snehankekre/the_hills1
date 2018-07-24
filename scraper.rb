d43bbcf5d0534da030122f288ac50fd67c5f2074
Snehan Kekre <snehan@minerva.kgi.edu>
2018-07-24 20:06:19 +0800

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,8 @@ require 'mechanize'
 require 'scraperwiki'
 
 agent = Mechanize.new
-agent.ssl_version = :TLSv1
+options_mask = OpenSSL::SSL::OP_NO_SSLv2 + OpenSSL::SSL::OP_NO_SSLv3 + OpenSSL::SSL::OP_NO_COMPRESSION
+agent.Net::HTTP::SSL_IVNAMES = options_mask
 enquiry_url = "https://epathway.thehills.nsw.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx"
 
 # Get the main page and ask for DAs
